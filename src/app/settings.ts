612c118e8e27c7d57de00adee5c0cbcae923caf4
mg <mg6625@gmail.com>
2023-01-17 23:44:07 +0300

    new pool form #1

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -1,3 +1,7 @@
-export const HARDHAT_ID: number = 1337
+export const HARDHAT_ID = 1337
 
 export const allowedNetworks: number[] = [HARDHAT_ID]
+
+export const EVM_ADDRESS_REGEXP = /^0x[a-fA-F0-9]{40}$/g
+
+export const MIN_POOL_AMOUNT = 1000
