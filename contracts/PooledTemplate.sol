df6c2b604b6a862a2cb63b6ec448ef02a913e3e1
mg <mg6625@gmail.com>
2023-01-30 22:37:33 +0300

    contracts upd

--- contracts/PooledTemplate.sol
+++ contracts/PooledTemplate.sol
@@ -2,9 +2,106 @@
 pragma solidity ^0.8.0;
 
 contract PooledTemplate {
-  address public creator;
+  enum Statuses {
+    Active,
+    Finished
+  }
 
-  constructor(address _creator) {
+  struct Participant {
+    address account;
+    uint256 share;
+    uint256 claimed;
+    string telegramId;
+    string twitterId;
+  }
+
+  struct ParticipantData {
+    uint256 share;
+    uint256 claimed;
+    string telegramId;
+    string twitterId;
+  }
+
+  address public creator;
+  string private name;
+  address private tokenAddress;
+  string private tokenName;
+  uint256 private tokenAmount;
+  Statuses public status;
+  uint256 private participantsCount;
+
+  mapping(address => ParticipantData) private participantsData;
+  mapping(uint256 => address) private mapper;
+
+  modifier onlyCreator() {
+    require(msg.sender == creator, "Only for creator");
+    _;
+  }
+
+  modifier atStatus(Statuses _status) {
+    require(status == _status, "Wrong status");
+    _;
+  }
+
+  constructor(
+    address _creator,
+    string memory _name,
+    address _tokenAddress,
+    string memory _tokenName,
+    uint256 _tokenAmount,
+    Participant[] memory _participants
+  ) {
     creator = _creator;
+    name = _name;
+    tokenAddress = _tokenAddress;
+    tokenName = _tokenName;
+    tokenAmount = _tokenAmount;
+    status = Statuses.Active;
+    addParticipants(_participants);
+  }
+
+  function addParticipants(Participant[] memory _participants) internal {
+    for (uint256 i; i < _participants.length; i++) {
+      Participant memory item = _participants[i];
+      participantsData[item.account] = ParticipantData(
+        item.share,
+        0,
+        item.telegramId,
+        item.twitterId
+      );
+      mapper[participantsCount] = item.account;
+      participantsCount++;
+    }
+  }
+
+  function getData()
+    external
+    view
+    returns (
+      address _creator,
+      string memory _name,
+      address _tokenAddress,
+      string memory _tokenName,
+      uint256 _tokenAmount,
+      Statuses _status,
+      Participant[] memory _participants
+    )
+  {
+    _participants = new Participant[](participantsCount);
+
+    for (uint256 i = 1; i <= participantsCount; i++) {
+      ParticipantData memory itemData = participantsData[mapper[i]];
+      Participant memory item = Participant(
+        mapper[i],
+        itemData.share,
+        itemData.claimed,
+        itemData.telegramId,
+        itemData.twitterId
+      );
+
+      _participants[i] = item;
+    }
+
+    return (creator, name, tokenAddress, tokenName, tokenAmount, status, _participants);
   }
 }
--- contracts/TrustedPool.sol
+++ contracts/TrustedPool.sol
@@ -5,10 +5,10 @@ import "hardhat/console.sol";
 import "./PooledTemplate.sol";
 
 contract TrustedPool {
-  mapping(address => address) internal pooledContracts; //creator -> contract
-
-  string internal data = "123";
   address public owner;
+
+  mapping(address => address) internal pooledContracts; //creator -> contract
+  mapping(address => address[]) internal participants; //participant -> contract
 
   modifier onlyOwner() {
     require(msg.sender == owner, "Only for owner");
@@ -19,15 +19,27 @@ contract TrustedPool {
     owner = msg.sender;
   }
 
-  function createPooledContract(address _creator) external {
-    pooledContracts[_creator] = new PooledTemplate(_creator);
+  function createPooledContract(
+    string memory _name,
+    address _tokenAddress,
+    string memory _tokenName,
+    uint256 _tokenAmount,
+    PooledTemplate.Participant[] memory _participants
+  ) external {
+    pooledContracts[msg.sender] = address(
+      new PooledTemplate(msg.sender, _name, _tokenAddress, _tokenName, _tokenAmount, _participants)
+    );
   }
 
-  function getPooledContractAddress(address _creator) external returns (address) {
+  function saveParticipants(PooledTemplate.Participant[] memory _participants) internal {
+    for (uint256 i; i < _participants.length; i++) {
+      if (participants[_participants[i].account].length > 0) {} else {
+        participants[_participants[i].account] = [pooledContracts[msg.sender]];
+      }
+    }
+  }
+
+  function getPooledContractAddress(address _creator) external view returns (address) {
     return pooledContracts[_creator];
-  }
-
-  function getData() external view onlyOwner returns (string memory) {
-    return data;
   }
 }
