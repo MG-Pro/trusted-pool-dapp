1b8ed805e3e35622e80ae9509e090ff409818068
mg <mg6625@gmail.com>
2023-02-12 21:15:21 +0300

    PooledTemplate: distribution in process

--- contracts/PooledTemplate.sol
+++ contracts/PooledTemplate.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract PooledTemplate {
   enum Statuses {
@@ -11,29 +11,28 @@ contract PooledTemplate {
 
   struct Participant {
     address account;
-    uint share;
-    uint claimed;
-    uint accrued;
+    uint256 share;
+    uint256 claimed;
+    uint256 accrued;
     string description;
   }
 
   struct ParticipantData {
-    uint share;
-    uint claimed;
-    uint ratio;
-    uint accrued;
+    uint256 share;
+    uint256 claimed;
+    uint256 accrued;
     string description;
   }
 
+  address private poolOrganizer;
   address private poolCreator;
-  string private poolName;
   address private poolTokenAddress;
+  string private poolName;
   string private poolTokenName;
-  Statuses private poolStatus;
 
-  uint private participantsCount;
-  uint private distributedBalance;
-  uint private poolTokenAmount;
+  uint256 private participantsCount;
+  uint256 private poolTokenAmount;
+  Statuses private poolStatus;
 
   mapping(address => ParticipantData) private participantsData;
   mapping(uint256 => address) private mapper;
@@ -58,6 +57,11 @@ contract PooledTemplate {
     _;
   }
 
+  modifier isContract(address _address) {
+    require(_address.code.length != 0, "Argument is not contract address");
+    _;
+  }
+
   constructor(
     address _creator,
     string memory _name,
@@ -74,16 +78,12 @@ contract PooledTemplate {
     addParticipants(_participants);
   }
 
-  function addParticipants(Participant[] memory _participants) internal {
+  function addParticipants(Participant[] memory _participants) private {
     for (uint256 i; i < _participants.length; i++) {
       Participant memory item = _participants[i];
-      // TODO: CHECK IT!
-      uint ratio = (item.share * 100) / poolTokenAmount;
-
       participantsData[item.account] = ParticipantData(
         item.share,
         item.claimed,
-        ratio,
         item.accrued,
         item.description
       );
@@ -92,68 +92,75 @@ contract PooledTemplate {
     }
   }
 
-  function getData()
-    public
+  function getPoolData(
+    uint256 first,
+    uint256 size
+  )
+    external
     view
     returns (
       address creator,
       string memory name,
       address tokenAddress,
       string memory tokenName,
+      uint256 tokenAmount,
       Statuses status,
       Participant[] memory participants
     )
   {
-    return (poolCreator, poolName, poolTokenAddress, poolTokenName, poolStatus, getParticipants());
+    require(participantsCount > first, '"first" more than count of participants');
+    if (participantsCount > size) {
+      size = participantsCount;
+    }
+
+    return (
+      poolCreator,
+      poolName,
+      poolTokenAddress,
+      poolTokenName,
+      poolTokenAmount,
+      poolStatus,
+      getParticipants(first, size)
+    );
   }
 
-  function setTokenAddress(address _tokenAddress) external onlyParticipant {
+  function setTokenAddress(
+    address _tokenAddress
+  ) external onlyParticipant isContract(_tokenAddress) {
     require(poolTokenAddress == address(0), "Token address already set");
-    require(isContract(_tokenAddress), "Argument is not contract address");
     poolTokenAddress = _tokenAddress;
   }
 
-  function tokenBalance() public view hasTokenAddress returns (uint256) {
-    return ERC20(poolTokenAddress).balanceOf(address(this));
-  }
-
-  function tokenDistribution() public {
-    uint balance = tokenBalance();
-    require(balance > distributedBalance, "There are not tokens for distribution");
-
-    uint undistributedBalance = balance - distributedBalance;
-    Participant[] memory _participants = getParticipants();
-
-    for (uint i = 0; i < _participants.length; i++) {
-      ParticipantData memory item = participantsData[_participants[i].account];
-      participantsData[_participants[i].account].accrued =
-        (undistributedBalance * item.ratio) /
-        100;
-    }
-    distributedBalance = balance;
+  function tokenBalance() public view hasTokenAddress onlyParticipant returns (uint256) {
+    return IERC20(poolTokenAddress).balanceOf(address(this));
   }
 
   function claimTokens() external hasTokenAddress {
     require(participantsData[msg.sender].accrued > 0, "There are not tokens for claim");
-    uint accrued = participantsData[msg.sender].accrued;
+    uint256 accrued = participantsData[msg.sender].accrued;
     participantsData[msg.sender].claimed += accrued;
     participantsData[msg.sender].accrued = 0;
 
-    bool success = ERC20(poolTokenAddress).transfer(address(this), accrued);
+    bool success = IERC20(poolTokenAddress).transfer(address(this), accrued);
     require(success, "Claim error");
   }
 
-  function getParticipants() private view returns (Participant[] memory) {
-    Participant[] memory participants = new Participant[](participantsCount);
-    for (uint i = 0; i < participantsCount; i++) {
+  function getParticipants(
+    uint256 first,
+    uint256 size
+  ) private view returns (Participant[] memory) {
+    if (participantsCount == 0) {
+      return new Participant[](0);
+    }
+
+    uint256 overallBalance = tokenBalance();
+    Participant[] memory participants = new Participant[](size);
+
+    for (uint256 i = first; i < size; i++) {
       ParticipantData memory data = participantsData[mapper[i]];
-      participants[i] = Participant(
-        mapper[i],
-        data.share,
-        data.claimed,
-        data.accrued,
-        data.description
-      );
+      uint256 accrued = (overallBalance * data.share) / poolTokenAmount - data.claimed;
+
+      participants[i] = Participant(mapper[i], data.share, data.claimed, accrued, data.description);
     }
 
     return participants;
@@ -161,13 +168,9 @@ contract PooledTemplate {
 
   function calculateTokenAmount(
     Participant[] memory _participants
-  ) private pure returns (uint sum) {
+  ) private pure returns (uint256 sum) {
     for (uint256 i; i < _participants.length; i++) {
       sum += _participants[i].share;
     }
-  }
-
-  function isContract(address _address) private view returns (bool) {
-    return _address.code.length != 0;
   }
 }
